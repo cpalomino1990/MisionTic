14284f9ac8c3d2bb179f112890ab724744570315
cpalomino1990 <carlospalominoes@gmail.com>
2021-09-22 06:55:53 -0500

    Fists commit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/rutas.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aviones{
+        public int id { get; set; }
+        public string marca { get; set; }
+        public int modelo { get; set; }
+        public string numero_asientos { get; set; }
+        public string numero_baños { get; set; }
+        public int capacidad_maxima { get; set; }
+    }
+
+}
